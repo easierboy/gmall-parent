c6b3929eb1bb731d320f852d3ed162ef442f5d4f
dell <1277838530@qq.com>
2022-08-24 16:03:07 +0800

    图片完成，改版

--- service/service-product/src/main/java/com/atguigu/gmall/product/controller/FileUploadController.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/controller/FileUploadController.java
@@ -12,6 +12,10 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.UUID;
 
 /**
@@ -40,9 +44,6 @@ public class FileUploadController {
      */
     @PostMapping("/fileUpload")
     public Result fileUpload(@RequestPart("file")MultipartFile file){
-        System.out.println(endpointUrl);
-        System.out.println(accessKey);
-        System.out.println(secreKey);
 
         try {
             //使用 MinIO 服务器游乐场、其访问密钥和密钥创建一个 minioClient
@@ -65,16 +66,24 @@ public class FileUploadController {
             //  定义一个文件的名称:名称不能重复！
             String fileName = System.currentTimeMillis() + UUID.randomUUID().toString() + file.getOriginalFilename();
 
-            // 将“/home/user/Photos/asiaphotos.zip”作为对象名称“asiaphotos-2015.zip”上传到存储桶
+            //方式二:文件上传uploadObject（只能用于普通文件File）
+//            //调用工具类将MultipartFile 转File
+//            File file1 = MultipartFileToFileUtils.multipartFileToFile(file);
+//            //删除本地临时文件
+//            MultipartFileToFileUtils.delteTempFile(file1);
+//            // 将“/home/user/Photos/asiaphotos.zip”作为对象名称“asiaphotos-2015.zip”上传到存储桶
 //            minioClient.uploadObject(
 //                    UploadObjectArgs.builder()
 //                            .bucket(bucketName)
 //                            .object(fileName)
-//                            .filename(file.getOriginalFilename())
+//                            .filename(file1.getName())
 //                            .build());
+            //方式一:文件上传putObject
             minioClient.putObject(
-                    PutObjectArgs.builder().bucket(bucketName).object(fileName).stream(
-                                    file.getInputStream(), file.getSize(), -1)
+                    PutObjectArgs.builder()
+                            .bucket(bucketName)
+                            .object(fileName)
+                            .stream(file.getInputStream(), file.getSize(), -1)
                             .contentType(file.getContentType())
                     .build());
 
@@ -84,5 +93,57 @@ public class FileUploadController {
             e.printStackTrace();
         }
        return Result.fail();
+    }
+}
+
+//MultipartFile 转File工具类
+class MultipartFileToFileUtils {
+
+    /**
+     * MultipartFile 转 File
+     *
+     * @param file
+     * @throws Exception
+     */
+    public static File multipartFileToFile(MultipartFile file) throws Exception {
+
+        File toFile = null;
+        if (file.equals("") || file.getSize() <= 0) {
+            file = null;
+        } else {
+            InputStream ins = null;
+            ins = file.getInputStream();
+            toFile = new File(file.getOriginalFilename());
+            inputStreamToFile(ins, toFile);
+            ins.close();
+        }
+        return toFile;
+    }
+
+    //获取流文件
+    private static void inputStreamToFile(InputStream ins, File file) {
+        try {
+            OutputStream os = new FileOutputStream(file);
+            int bytesRead = 0;
+            byte[] buffer = new byte[8192];
+            while ((bytesRead = ins.read(buffer, 0, 8192)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            os.close();
+            ins.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 删除本地临时文件
+     * @param file
+     */
+    public static void delteTempFile(File file) {
+        if (file != null) {
+            File del = new File(file.toURI());
+            del.delete();
+        }
     }
 }
