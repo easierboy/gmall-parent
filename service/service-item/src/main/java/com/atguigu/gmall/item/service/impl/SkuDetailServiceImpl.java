8d9d674fa6cbb651dc29de73aa483c78536f882d
dell <1277838530@qq.com>
2022-08-28 19:40:35 +0800

    添加异步编排

--- service/service-item/src/main/java/com/atguigu/gmall/item/service/impl/SkuDetailServiceImpl.java
+++ service/service-item/src/main/java/com/atguigu/gmall/item/service/impl/SkuDetailServiceImpl.java
@@ -25,6 +25,7 @@ import java.util.concurrent.ThreadPoolExecutor;
  * @Date 2022/8/26 23:08
  */
 @Service
+@EnableThreadPool
 public class SkuDetailServiceImpl implements SkuDetailService {
 
     @Autowired
